30e310df6094cb6635d2444005875fb96f4cd584
MohamedAdel510 <119543542+MohamedAdel510@users.noreply.github.com>
2024-06-27 03:29:54 +0300

    Add project files.

---
+++ Assignment 03 part 2/Program.cs
@@ -0,0 +1,322 @@
+﻿using System.Numerics;
+
+namespace Assignment_03_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q17
+            /*
+             * Suppose the points are A, B and C. Find the slope of the line passing 
+             * through A and B. Find the Slope of the line passing through B and C. 
+             * If the slopes are the same, the points are on the same line.
+             * if A (x1, y1) and B(x2, y2) then slope of AB is (y1-y2)/(x1-x2) 
+             */
+            //double x1, y1, x2, y2, x3, y3, slop1, slop2;
+            //x1 = double.Parse(Console.ReadLine());
+            //y1 = double.Parse(Console.ReadLine());
+            //x2 = double.Parse(Console.ReadLine());
+            //y2 = double.Parse(Console.ReadLine());
+            //x3 = double.Parse(Console.ReadLine());
+            //y3 = double.Parse(Console.ReadLine());
+
+            //slop1 = (y1 - y2) / (x1 - x2);
+            //slop2 = (y2 - y3) / (x2 - x3);
+
+            //if(slop1 == slop2)
+            //{
+            //    Console.WriteLine("these points lie on a single straight line");
+            //}
+            #endregion
+
+            #region Q18
+            //Console.WriteLine("Enter the time taken for the task: ");
+            //int WorkHours = int.Parse(Console.ReadLine());
+
+            //if (WorkHours >= 2 && WorkHours <= 3)
+            //    Console.WriteLine("highly efficient");
+            //else if (WorkHours > 3 && WorkHours <= 4)
+            //    Console.WriteLine("instructed to increase their speed");
+            //else if (WorkHours > 4 && WorkHours <= 5)
+            //    Console.WriteLine("provided with training to enhance their speed. ");
+            //else
+            //    Console.WriteLine("they are required to leave the company.");
+            #endregion
+
+            #region Q19
+            //Console.WriteLine("Enter size of idntity matrix: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == j)
+            //            Console.Write(" 1 ");
+            //        else
+            //            Console.Write(" 0 ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Q20
+            //const int size = 100;
+            //int[] arr = new int[size];
+
+            //Console.Write("Enter number of array element: ");
+            //int numOfElement = Convert.ToInt32(Console.ReadLine());           
+            //int sum = 0;
+
+            //for (int i = 0; i < numOfElement; i++)
+            //{
+            //    Console.Write($"element{i+1}: ");
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //    sum += arr[i];
+            //}
+            //Console.WriteLine($"Sum of array element: {sum}");
+            #endregion
+
+            #region Q21
+            //int[] arr1 = new int[100];
+            //int[] arr2 = new int[100];
+            //int[] arr3 = new int[200];
+
+            //int s1, s2, s3, tmp;
+
+            //Console.WriteLine("Enter size of first array: ");
+            //s1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter array elements: ");
+            //Console.WriteLine("----------------");
+            //for (int i = 0; i < s1; i++) {
+            //    arr1[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("================");
+
+            //Console.WriteLine("Enter size of second array: ");
+            //s2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter array elements: ");
+            //Console.WriteLine("----------------");
+            //for (int i = 0; i < s2; i++)
+            //{
+            //    arr2[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //s3 = s1 + s2;
+
+            //int k = 0;
+            //while( k < s1 )
+            //{
+            //    arr3[k] = arr1[k];
+            //    k++;
+            //}
+
+            //for(int j  = 0; j < s2; j++)
+            //{
+            //    arr3[k] = arr2[j];
+            //    k++;
+            //}
+
+            //// sorting arr3 using bubble sort
+            //for(int i = 0; i < s3 - 1; i++)
+            //{
+            //    for(int j = 0;j < s3 - 1; j++)
+            //    {
+            //        if (arr3[j] > arr3[j + 1])
+            //        {
+            //            tmp = arr3[j + 1];
+            //            arr3[j + 1] = arr3[j];
+            //            arr3[k] = tmp;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("After merge: ");
+            //for (int i = 0; i < s3; i++) {
+            //    Console.WriteLine(arr3[i]);
+            //}
+            #endregion
+
+            #region Q22
+            //const int size = 100;
+            //int[] arr = new int[size];
+
+            //Console.WriteLine("Enter number of element: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //int[] freq = new int[n];
+
+            //Console.WriteLine("Enter array element: ");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arr[i] = Convert.ToInt32(Console.ReadLine()); // 25 12 43 25 10 
+            //}
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    int count = 1;
+            //    for (int j = i + 1; j < n; j++)
+            //    {
+            //        if (arr[i] == arr[j])
+            //        {
+            //            count++;
+            //            freq[j] = -1;
+            //        }
+            //    }
+            //    if (freq[i] != -1)
+            //    {
+            //        freq[i] = count;
+            //    }
+            //}
+
+            //Console.WriteLine("=============");
+            //for(int i = 0;i < n; i++)
+            //{
+            //    if (freq[i] != -1)
+            //    {
+            //        Console.WriteLine($"{arr[i]} Occers {freq[i]} times");
+            //    }
+            //}
+            #endregion
+
+            #region Q23
+            //int[] arr = new int[5];
+            //int max = int.MinValue, min = int.MaxValue;
+
+            //Console.WriteLine("Enter array element: ");
+            //for(int i =  0; i < arr.Length; i++)
+            //{
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] > max)
+            //        max = arr[i];
+            //    if (arr[i] < min)
+            //        min = arr[i];
+            //}
+            //Console.WriteLine($"Maximum element: {max}");
+            //Console.WriteLine($"Minimum element: {min}");
+
+            #endregion
+
+            #region Q24
+            //int[] arr = new int[5];
+            //int max = int.MinValue, secMax = int.MinValue;
+
+            //Console.WriteLine("Enter array element: ");
+            //for(int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //foreach (int i in arr)   
+            //{
+            //    if (i > max)
+            //    {
+            //        secMax = max;
+            //        max = i;
+            //    }
+            //    else if (i > secMax && i != max)
+            //    { 
+            //        secMax = i;
+            //    }
+            //}
+
+            //System.Console.WriteLine($"second largest element: {secMax}");
+
+            #endregion
+
+            #region Q25
+            ////  ----------------8------------------
+            //// |    --------------6------------    |
+            //// |   |                           |   |
+            ////[7 | 0 | 0 | 0 | 5 | 6 | 7 | 5 | 0 | 7 | 5 | 3 ]
+            ////                 |                       |
+            ////                  -----------5-----------
+
+            ////const int size = 100;
+            //Console.WriteLine("Enter number of element: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[] arr = new int[size];
+            //int[] distances = new int[size];
+            //int longest = int.MinValue;
+
+            //for (int i = 0; i < size; i++)
+            //{
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //for(int i = 0; i < size; i++)
+            //{
+            //    int cell = 0;
+            //    for(int j = i + 1; j < size; j++)
+            //    {
+            //        if (arr[i] == arr[j])
+            //            cell = j - (i+1);
+            //    }
+            //    distances[i] = cell;
+            //    if (distances[i] > longest)
+            //        longest = distances[i];
+            //}
+
+            //Console.WriteLine($"Longest distance: {longest} cells");
+            #endregion
+
+            #region Q26
+            //string str = Console.ReadLine();
+            //char[] spearator = {' '};
+            //int count = str.Length;
+            //string[] strList = str.Split(spearator, count, StringSplitOptions.None);
+            //for(int i = strList.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write($"{strList[i]} ");
+            //}
+            #endregion
+
+            #region Q27
+            //Console.WriteLine("Enter the size: ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[,] firstArray = new int[size, size];
+            //int[,] secArray = new int[size, size];
+
+            //Console.WriteLine("Enter array element: ");
+            //for(int i = 0; i < size; i++)
+            //{
+            //    for(int j = 0; j < size; j++)
+            //    {
+            //        firstArray[i, i] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+
+            //for (int i = 0; i < size; i++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        secArray[i, j] = firstArray[i, j];
+            //    }
+            //}
+
+            //Console.WriteLine("Element of seconed array: ");
+            //for(int i = 0; i <size; i++)
+            //{
+            //    for (int j = 0;j < size; j++)
+            //    {
+            //        Console.Write($"{secArray[i, j]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Q28
+            //int[] arr = { 1, 2, 3, 4, 5, 6 };
+            //for(int i = arr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+            #endregion
+        }
+    }
+}
---
+++ Demo 03 part 2/Program.cs
@@ -0,0 +1,218 @@
+﻿using System.Text;
+
+namespace Demo_03_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Control statements [for - foreach - while - do while]
+
+            #region [for - foreach]
+            // if you asked to print from 1 to 10 what is your code?
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+            //Console.WriteLine(4);
+            //Console.WriteLine(5);
+            //Console.WriteLine(6);
+            //Console.WriteLine(7);
+            //Console.WriteLine(8);
+            //Console.WriteLine(9);
+            //Console.WriteLine(10);
+            // what if printing from 1 to 1000!
+            // why loops:
+
+            ////      1          2       4
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);  // 3
+            //}
+
+
+            // for array or array list we can use foreach 
+            // but we can not do any operation on index or use control statement like (continue, break)
+            //int[] numbers = { 1, 2, 3, 4, 5 };
+
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            #endregion
+
+            #region [while - do while]
+            // while loop same as for only different in syntax
+
+            //int i = 1;                     // 1 
+            //while (i <= 10)                // 2
+            //{ 
+            //    Console.WriteLine(i);      // 3
+            //    i++;                       // 4
+            //}
+
+            // do while different from for loop and while loop 
+            // the statement will be execute at lest only one time befor checking the condition
+
+            // program to acept only positve number from the user
+
+            //int number;
+            //bool flag = false;
+            //do {
+            //    Console.WriteLine("Please Enter Positive Number: ");
+            //    //number  = int.Parse(Console.ReadLine());
+            //    flag = int.TryParse(Console.ReadLine(), out number);
+            //} while (number <= 0 || !flag);
+            //Console.WriteLine($"{number} is positive");
+            #endregion
+
+
+            #region String vs StringBuilder
+            //string name01;
+            //// declear reference from type string
+            //// allow 4 byte at stack for reference object
+            //// allocate 0 bytes at heap
+            //// reference to defualt value null
+            //name01 = new string("nouwara");
+            //// allocate required byts in heap
+            //// intialize value nouwara
+            //// call user defined constructor
+            //// assigen reference to allocate object
+
+            //string name02 = "Mohamed"; //syntax sugar
+            //name02 = name01;
+
+            //name01 = "Adel";
+            //Console.WriteLine(name01);
+            //Console.WriteLine(name02);
+
+            //// string is an imutable 
+
+
+            //string message = "Hello";
+            //Console.WriteLine(message);
+            //Console.WriteLine(message.GetHashCode());
+            //message += " Route";
+            //Console.WriteLine(message);
+            //Console.WriteLine(message.GetHashCode());
+
+            //// string builder
+            ////----------------
+            //StringBuilder SB = new StringBuilder();
+            //SB.Append("Hello");
+            //Console.WriteLine(SB);
+            //Console.WriteLine(SB.GetHashCode());
+            //SB.Append(" Route!");
+            //Console.WriteLine(SB);
+            //Console.WriteLine(SB.GetHashCode());
+            #endregion
+
+            #region 1D Array
+            //// if you want to store 3 numbers what is your code
+            //int x, y, z;
+            //x = int.Parse(Console.ReadLine());
+            //y = int.Parse(Console.ReadLine());
+            //z = int.Parse(Console.ReadLine());
+
+            //// why Array?
+            //// now if you want to store 1000 number what is your code
+
+            //// intializing and declearing an arry
+
+            //int[] numbers = new int[3];
+            //numbers[0] = 1;
+            //numbers[1] = 2;
+            //numbers[2] = 3;
+
+            //int[] numbers2 = new int[3] { 1, 2, 3,};
+
+            //int[] numbers3 = new int[] { 1, 2, 3, 4, 5 };
+            //Console.WriteLine(numbers3.Length); // to git size of array
+
+            //int[] numbers4 = { 1, 2, 3 };
+            //Console.WriteLine(numbers4.Length);
+            //Console.WriteLine(numbers4.Rank); // to git dimentions of array
+
+            //// filling and printing array element
+
+            //const int size = 100; // size of an array must be constant
+            //int[] numbers5 = new int[size];
+
+            //Console.Write("Num Of Array Element: ");
+            //int NumOfElement = int.Parse(Console.ReadLine());
+
+            //for(int i = 0; i < NumOfElement; i++)
+            //{
+            //    Console.Write($"Element {i + 1}: ");
+            //    numbers5[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("===============");
+            //Console.WriteLine("Array Elements");
+            //Console.WriteLine("===============");
+
+            //for(int i = 0; i < NumOfElement; i++)
+            //{
+            //    Console.WriteLine(numbers5[i]);
+            //}
+            #endregion
+
+            #region 2D Array
+            //// initalizing and declearing 2D Array
+            //int[,] marks = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9} };
+            //Console.WriteLine(marks.GetLength(0));  // Rows
+            //Console.WriteLine(marks.GetLength(1));  // Columns
+
+            //// filling and printing 2D array elements
+            //const int rows = 3, columns = 5;
+            //int[,] marks = new int[rows, columns];
+
+            //for (int i = 0; i < marks.GetLength(0) /*or i < rows*/; i++) {
+            //    Console.WriteLine($"Student {i + 1}: ");
+            //    Console.WriteLine("----------");
+            //    for (int j = 0; j < marks.GetLength(1); j++)
+            //    {
+            //        Console.Write($"Subject {j + 1}: ");
+            //        marks[i, j] = int.Parse(Console.ReadLine());
+            //    }
+            //    Console.WriteLine("------------");
+            //}
+            //Console.WriteLine("===============");
+            //Console.WriteLine("Student grades: ");
+            //Console.WriteLine("===============");
+
+            //for (int i = 0;i < marks.GetLength(0) ; i++)
+            //{
+            //    for (int j = 0;j < marks.GetLength(1); j++)
+            //    {
+            //        Console.Write($"{marks[i, j]}  ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Boxing & Unboxing
+            // Boxing : Casting from datatybe [value type] to datatybe [reference type]
+            // Unboxing: Casting from datatybe [reference type] to datatype [value type]
+            object obj;
+            // declare reference of tupe object refering to null
+            // this reference obj can refer to unstance from type object or any datatype 
+            obj = new object();
+            obj = new string("Mohamed"); // implicit casting or boxing
+            obj = 'A'; 
+            obj = 3;
+            obj = true;
+            bool flag = (bool)obj; //Explicit casting or Unboxing or unsafe casting
+
+            int x = 5;
+            object o1 = x; // casting from int[value type] to object [referenc type] 
+            Console.WriteLine(o1);
+
+            object o2 = 10;
+            o2 = "Ahmed";
+            int y = (int) o2; // unboxing 
+            #endregion
+
+            #endregion
+        }
+    }
+}
